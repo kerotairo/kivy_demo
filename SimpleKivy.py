1bc8586e9aaaa7f654718989aa8e408edc1df1e7
kevin <kerotairo@live.com>
2019-02-28 15:28:41 +0800

    Adding dynamic position for buttons

--- SimpleKivy.py
+++ SimpleKivy.py
@@ -3,7 +3,13 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.lang.builder import Builder
+from kivy.uix.widget import Widget
 
+class Widgets(Widget):
+    pass
+
+Builder.load_file('./SimpleKivy.kv')
 class LoginScreen(GridLayout):
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
@@ -17,11 +23,15 @@ class LoginScreen(GridLayout):
         self.password = TextInput(multiline=False, password=True)
         self.add_widget(self.password)
 
+        self.add_widget(Label(text="Two Factor Auth:"))
+        self.tfa = TextInput(multiline=False)
+        self.add_widget(self.tfa)
+
 
 
 class SimpleKivy(App):
     def build(self):
-        return LoginScreen()
+        return Widgets()
 
 if __name__ == "__main__":
     SimpleKivy().run()
